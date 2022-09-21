872437234fa1cb28feffbf1b1911dbe9e99ef439
Jack Atkinson <jacson@dhcp-172-24-140-60.eduroam.wireless.private.cam.ac.uk>
2022-09-21 10:37:41 +0100

    reformatted using black

--- binary-search-typed.py
+++ binary-search-typed.py
@@ -2,11 +2,15 @@
 import math
 from typing import Optional
 
+
 def binary_search(l: list[int], x: int) -> Optional[int]:
     return binary_search_inner(l, x, 0, len(l) - 1)
 
+
 # take care for the awkward indexing (floor and idx_left are coupled in a surprising way)
-def binary_search_inner(l: list[int], x: int, idx_left: int, idx_right: int) -> Optional[int]:
+def binary_search_inner(
+    l: list[int], x: int, idx_left: int, idx_right: int
+) -> Optional[int]:
     if idx_left == idx_right:
         if x == l[idx_left]:
             return idx_left
@@ -15,12 +19,11 @@ def binary_search_inner(l: list[int], x: int, idx_left: int, idx_right: int) -> 
     else:
         idx = math.floor((idx_left + idx_right) / 2)
         if x < l[idx]:
-            return binary_search_inner(l, x, idx_left, idx-1)
+            return binary_search_inner(l, x, idx_left, idx - 1)
         elif x > l[idx]:
-            return binary_search_inner(l, x, idx+1, idx_right)
-        else: # must be equal
+            return binary_search_inner(l, x, idx + 1, idx_right)
+        else:  # must be equal
             return idx
-
 
 
 print(binary_search([0, 1, 2, 3, 4, 5, 6], 0))
